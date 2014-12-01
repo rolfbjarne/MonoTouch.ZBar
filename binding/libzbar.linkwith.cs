1b8ce0b59d01942edb14701c28247ed5c325193d
Rolf Bjarne Kvinge <rolf@xamarin.com>
2014-12-01 13:41:14 +0100

    Build for arm64 as well.

--- binding/libzbar.linkwith.cs
+++ binding/libzbar.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith ("libzbar.a", LinkTarget.ArmV6 | LinkTarget.ArmV7 | LinkTarget.Simulator, ForceLoad = true, Frameworks = "CoreGraphics AVFoundation CoreMedia CoreVideo QuartzCore", LinkerFlags = "-liconv")]
+[assembly: LinkWith ("libzbar.a", LinkTarget.ArmV7 | LinkTarget.Simulator | LinkTarget.Arm64 | LinkTarget.Simulator64, ForceLoad = true, Frameworks = "CoreGraphics AVFoundation CoreMedia CoreVideo QuartzCore", LinkerFlags = "-liconv")]
